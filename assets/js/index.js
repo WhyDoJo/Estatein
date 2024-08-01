43aede009e8b9ad0f69bbeb26f5328aa5943f03f
WhyDojo <grinvaurdourin@gmail.com>
2024-08-01 16:21:35 +0300

    add script for swiper

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,20 @@
+// ======= TAGLINE
 import tagline from './modules/tagline.js'
 tagline()
+
+// ======= SWIPER
+const swiper = new Swiper('.swiper', {
+	slidesPerView: 3,
+	stretch: 0,
+
+	pagination: {
+		el: '.swiper-pagination',
+		type: 'fraction',
+	},
+
+	// Navigation arrows
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+})
