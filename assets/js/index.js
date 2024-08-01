dfb1022ca176137abf1978cacc5e868a5155b378
MEDOYED <mmax26072005@gmail.com>
2024-08-01 13:06:12 +0200

    customize own swiper pagination

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,14 +7,25 @@ const swiper = new Swiper('.swiper', {
 	slidesPerView: 3,
 	stretch: 0,
 
-	pagination: {
-		el: '.swiper-pagination',
-		type: 'fraction',
-	},
-
 	// Navigation arrows
 	navigation: {
 		nextEl: '.swiper-button-next',
 		prevEl: '.swiper-button-prev',
 	},
+})
+
+//  ======== swiper__number
+
+const firstNum = document.querySelector('.property__first-number');
+const btnNext = document.querySelector('.swiper-button-next');
+const btnPrev = document.querySelector('.swiper-button-prev');
+
+firstNum.textContent = Number(1);
+
+btnNext.addEventListener('click', function() {
+	firstNum.textContent = Number(firstNum.textContent) + 1;
+})
+
+btnPrev.addEventListener('click', () => {
+	firstNum.textContent = Number(firstNum.textContent) - 1;
 })
